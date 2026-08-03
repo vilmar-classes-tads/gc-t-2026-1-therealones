05bccc04ada4f68e28ec472c2078d99a8cb9d548
Pinmarigor <igor.vitor.ms00@gmail.com>
2026-08-02 21:27:20 -0300

    test: adiciona testes automatizados da Issue 1

---
+++ src/test/java/com/the_real_ones/unit/EditalRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.the_real_ones.unit;
+
+import com.the_real_ones.model.Edital;
+import com.the_real_ones.repository.EditalRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EditalRepositoryTest {
+
+    private EditalRepository editalRepository;
+
+    @BeforeEach
+    public void setup() {
+        editalRepository = new EditalRepository();
+    }
+
+    @Test
+    @DisplayName("CT-04: Listagem de editais retornando a lista vazia quando não há itens cadastrados")
+    public void testListarEditaisVazio() {
+        // Arrange (Setup inicializa repositório vazio)
+
+        // Act
+        ArrayList<Edital> list = editalRepository.readAll();
+
+        // Assert
+        assertNotNull(list);
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    @DisplayName("CT-05: Listagem de editais com sucesso quando existem itens cadastrados")
+    public void testListarEditaisComSucesso() {
+        // Arrange
+        LocalDate inicioSubmissao = LocalDate.of(2026, 8, 1);
+        LocalDate fimSubmissao = LocalDate.of(2026, 8, 10);
+        LocalDate inicioAvaliacao = LocalDate.of(2026, 8, 11);
+        LocalDate fimAvaliacao = LocalDate.of(2026, 8, 20);
+        
+        Edital edital1 = new Edital("Edital 1", 1, 2026, inicioSubmissao, fimSubmissao, inicioAvaliacao, fimAvaliacao);
+        Edital edital2 = new Edital("Edital 2", 2, 2026, inicioSubmissao, fimSubmissao, inicioAvaliacao, fimAvaliacao);
+        
+        editalRepository.create(edital1);
+        editalRepository.create(edital2);
+
+        // Act
+        ArrayList<Edital> list = editalRepository.readAll();
+
+        // Assert
+        assertNotNull(list);
+        assertEquals(2, list.size());
+        assertTrue(list.contains(edital1));
+        assertTrue(list.contains(edital2));
+    }
+}
